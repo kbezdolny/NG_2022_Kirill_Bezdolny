02f2120d79e2ae0cab8fa94a34c0a69c43f4f03b
Gubochka <newicraft2201@gmail.com>
2022-10-19 19:47:01 +0300

    Update task4 file

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,49 +1,18 @@
-import re
+a, b, c = int(input("Input a number: ")), int(input("Input b number: ")), int(input("Input c number: "))
 
-# 3x^2 - 10x + 3
-# 4x^2 + 21x + 5
-# x^2 - 2x - 3
-equation = input("Input your quadratic equation ( For test: 3x^2 - 10x + 3): ")
+# calculate a discriminant
+discriminant = b ** 2 - 4 * a * c
 
-# quadratic equation calculate function
-def quadraticEquation(num1: int, operator1: str, num2: int, operator2: str, num3: int):
-    # set arguments symbol
-    if operator1.replace(" ", "") == '-': num2 = -num2
-    if operator2.replace(" ", "") == '-': num3 = -num3
+# search discriminant values
+if discriminant > 0:
+    x1 = (-b + discriminant ** 0.5) / (a * 2)
+    x2 = (-b - discriminant ** 0.5) / (a * 2)
+    result = f"Quadratic equation resualt: x1 = {x1}; x2 = {x2}"
+elif discriminant == 0:
+    result = f"Quadratic equation resualt: x = {-b / (a * 2)}"
+elif discriminant < 0:
+    result = "Equation does not exist!"
+else:
+    result = "Equation invalid!"
 
-    # calculate a discriminant
-    discriminant = num2 ** 2 - 4 * num1 * num3
-
-    # search discriminant values
-    if discriminant > 0:
-        x1 = (-num2 + discriminant ** 0.5) / (num1 * 2)
-        x2 = (-num2 - discriminant ** 0.5) / (num1 * 2)
-        result = f"Quadratic equation resualt: X1 = {x1}; X2 = {x2}"
-    elif discriminant == 0:
-        result = f"Quadratic equation resualt: X = {-num2 / (num1 * 2)}"
-    elif discriminant < 0:
-        result = "Equation does not exist!"
-    else:
-        result = "Equation invalid!"
-
-    return result
-
-# remove unnecessary elements of the equation
-try:
-    pattern = ['*', '^2', 'x', '(', ')']
-    for i in range(0, len(pattern)):
-        equation = equation.replace(pattern[i], '')
-
-    # get everything element from equation
-    equationArguments = re.split(r'([^0-9]+)', equation)
-
-    if equationArguments[0] == "": equationArguments[0] = 1
-
-    # call quadratic equation calculate function
-    equationResualt = quadraticEquation(int(equationArguments[0]), equationArguments[1], int(equationArguments[2]),
-                                        equationArguments[3], int(equationArguments[4]))
-
-    # print quadratic equation result
-    print(equationResualt)
-except Exception as e:
-    print(e)
+print(result)
