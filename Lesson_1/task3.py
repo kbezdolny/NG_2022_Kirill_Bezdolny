1b1ed1d18dec70a70b189cfc2933c8763bf7320a
Gubochka <newicraft2201@gmail.com>
2022-10-19 19:37:53 +0300

    Update task3.py file

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,5 +1,22 @@
-import datetime
+secondsCount = int(input("Input second count: "))
+data = []
 
-secondCount = int(input("Input second count: "))
-dateTime = datetime.datetime.fromtimestamp(secondCount, tz=datetime.timezone.utc)
-print(dateTime.strftime("%d %H:%M:%S"))
+# calculate seconds
+data.append(secondsCount % 60)
+# calculate minutes
+data.append((secondsCount % 3600) // 60)
+# calculate hours
+data.append(secondsCount // 3600)
+# calculate days
+data.append(data[2] // 24)
+
+# set max 23 hours
+if data[2] > 23:
+    data[2] = data[2] % data[3]
+
+# add deco zero
+for i in range(0, len(data)-1):
+    if data[i] < 10:
+        data[i] = f"0{data[i]}"
+
+print(f"Day {data[3]} {data[2]}:{data[1]}:{data[0]}")
