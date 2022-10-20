b1c3a00714cc3c86dd7ec35133ead4350483499e
Gubochka <newicraft2201@gmail.com>
2022-10-20 15:23:04 +0300

    Update task3.py file

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -2,17 +2,13 @@ secondsCount = int(input("Input second count: "))
 data = []
 
 # calculate seconds
-data.append(secondsCount % 60)
+data.append(int(((secondsCount % 86400) % 3600) % 60))
 # calculate minutes
-data.append((secondsCount % 3600) // 60)
+data.append(int(((secondsCount % 86400) % 3600) / 60))
 # calculate hours
-data.append(secondsCount // 3600)
+data.append(int((secondsCount % 86400) / 3600))
 # calculate days
-data.append(data[2] // 24)
-
-# set max 23 hours
-if data[2] > 23:
-    data[2] = data[2] % data[3]
+data.append(int(secondsCount / 86400))
 
 # add deco zero
 for i in range(0, len(data)-1):
