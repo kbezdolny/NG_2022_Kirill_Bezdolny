1e2a6bd0e0ca61ab78b14c975e9384312873e660
Gubochka <newicraft2201@gmail.com>
2022-10-21 20:57:17 +0300

    Update task3.py file

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,18 +1,13 @@
 secondsCount = int(input("Input second count: "))
-data = []
 
 # calculate seconds
-data.append(((secondsCount % 86400) % 3600) % 60)
+seconds = ((secondsCount % 86400) % 3600) % 60
 # calculate minutes
-data.append(((secondsCount % 86400) % 3600) // 60)
+minutes = ((secondsCount % 86400) % 3600) // 60
 # calculate hours
-data.append((secondsCount % 86400) // 3600)
+hours = (secondsCount % 86400) // 3600
 # calculate days
-data.append(secondsCount // 86400)
+days = secondsCount // 86400
 
-# add deco zero
-for i in range(0, len(data)-1):
-    if data[i] < 10:
-        data[i] = f"0{data[i]}"
-
-print(f"Day {data[3]} {data[2]}:{data[1]}:{data[0]}")
+# output the datetime
+print(f"Day {days} {hours}:{minutes}:{seconds}")
